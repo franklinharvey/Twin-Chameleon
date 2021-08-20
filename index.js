9404f72e9174e22027796adab10755b7a29a5776
Franklin Harvey <franklin_harvey@mckinsey.com>
2021-08-20 09:40:02 -0600

    fix rendering

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const _x = document;
 const _y = "innerHTML";
-const _z = "getElementById";
-const _w = "createElement";
 const releaseDate = new Date("2021-08-20T04:00:00Z");
-const countDownDate = releaseDate.getTime();
+const countDownTime = releaseDate.getTime();
 render();
 
 // Update the count down every 1 second
-const interval = setInterval(function () {
+var interval = setInterval(function () {
   render();
 }, 1000);
 
-kill = () => {
+function kill() {
   clearInterval(interval);
-};
+}
 
 function render() {
   // Get today's date and time
@@ -21,7 +18,7 @@ function render() {
   const now = new Date().getTime();
 
   // Find the distance between now and the count down date
-  const distance = countDownDate - now;
+  const distance = countDownTime - now;
 
   // Time calculations for days, hours, minutes and seconds
   const days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -31,18 +28,19 @@ function render() {
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  const parent = _x[_z]("countdown");
+  const parent = document.getElementById("countdown");
+  const video = document.getElementById("video-embed");
   parent[_y] = "";
 
-  const countDownElem = _x[_w]("h1");
+  const countDownElem = document.createElement("h1");
   countDownElem[_y] =
     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-  const spacer = _x[_w]("br");
-  const releaseDateElem = _x[_w]("p");
+  const spacer = document.createElement("br");
+  const releaseDateElem = document.createElement("p");
   releaseDateElem[_y] = releaseDate.toDateString();
 
-  const releaseTimeElem = _x[_w]("small");
+  const releaseTimeElem = document.createElement("small");
   releaseTimeElem[_y] = releaseDate.toLocaleTimeString();
   parent.appendChild(countDownElem);
   parent.appendChild(spacer);
@@ -53,9 +51,9 @@ function render() {
   if (distance <= 0) {
     kill();
     parent.classList.add("hide");
-    _x[_z]("video-embed").classList.remove("hide");
+    video.classList.remove("hide");
   } else {
     parent.classList.remove("hide");
-    _x[_z]("video-embed").classList.add("hide");
+    video.classList.add("hide");
   }
 }
